0c22c0dfecb3ab67f02f4737a0ea72d7149fdbf8
sharpdlt <sharpdlt@gmail.com>
2022-06-11 12:43:06 +0300

    initial commit

---
+++ main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(
+        default=True, db_index=True, verbose_name='Прошел активацию?')
+    send_message = models.BooleanField(
+        default=True, verbose_name='Слать оповещения о новых комментариях?')
+
+    class Meta:
+        pass
---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+from django.contrib.auth.views import LoginView
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
