136a072e67cd1556bfe20589f1c7f78656ae92a3
sharpdlt <sharpdlt@gmail.com>
2022-06-13 14:19:20 +0300

    Activating an account with a letter Account deleting

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
 
-from .views import index, other_page, profile, BBLoginView, BBLogoutView, ChangeUserInfoView
+from .views import index, other_page, profile, user_activate
+from .views import BBLoginView, BBLogoutView
+from .views import ChangeUserInfoView, BBPasswordChangeView, DeleteUserView
+from .views import RegistreUserView, RegisterDoneView
+from .views import user_activate
+
 
 app_name = 'main'
 urlpatterns = [
-    path('accounts/profile/change/',
-         ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/register/activate/<str:sign>/',
+         user_activate, name='register_activate'),
+    path('accounts/register/done/',
+         RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegistreUserView.as_view(), name='register'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/password/change/',
+         BBPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
+    path('accounts/profile/change/',
+         ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
--- main/views.py
+++ main/views.py
@@ -1,18 +1,23 @@
-from django.http import Http404, HttpRequest, HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_list_or_404
 from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
+from django.core.signing import BadSignature
+from django.contrib import messages
+from django.contrib.auth import logout
 
-from .forms import ChangeUserInfoForm
+from .forms import ChangeUserInfoForm, RegisterUserForm
 
 from .models import AdvUser
+
+from .utilities import signer
 
 
 def index(request):
@@ -40,6 +45,12 @@ class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
 
 
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменен'
+
+
 class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = AdvUser
     template_name = 'main/change_user_info.html'
@@ -50,6 +61,54 @@ class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     def setup(self, request, *args, **kwargs):
         self.user_id = request.user.pk
         return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class RegistreUserView(CreateView):
+    model = AdvUser
+    template_name = 'main/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('main:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'main/register_done.html'
+
+
+def user_activate(request, sign):
+    try:
+        username = signer.unsign(sign)
+    except BadSignature:
+        return render(request, 'main/bad_signature.html')
+    user = get_object_or_404(AdvUser, username=username)
+
+    if user.is_activated:
+        template = 'main/user_is_activated.html'
+    else:
+        template = 'main/activation_done.html'
+        user.is_active = True
+        user.is_activated = True
+        user.save()
+    return render(request, template)
+
+
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+    model = AdvUser
+    template_name = 'main/delete_user.html'
+    success_url = reverse_lazy('main:index')
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.add_message(request, messages.SUCCESS, 'Пользователь удален')
+        return super().post(request, *args, **kwargs)
 
     def get_object(self, queryset=None):
         if not queryset:
